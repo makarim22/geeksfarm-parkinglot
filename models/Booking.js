64a932b23901e5eb7e6390893c1a04d0e6663a9c
makarim muhammad <jakamakarim@gmail.com>
2026-04-03 05:26:18 +0700

    feat : change booking id

--- models/Booking.js
+++ models/Booking.js
@@ -6,42 +6,48 @@ class Booking extends Model {
         Booking.belongsTo(models.User, { foreignKey: 'userId' }); // Defines the many-to-one relationship  
         Booking.belongsTo(models.ParkingLot, { foreignKey: 'parkingLotId' }); // Assume ParkingLot table exists  
     }  
-}  
-Booking.init({  
-    parkingLotId: {  
-        type: DataTypes.INTEGER,  
-        allowNull: false,  
-    },  
-    vehicleId: {  
-        type: DataTypes.INTEGER,  
-        allowNull: false,  
-    },  
-    userId: {  
-        type: DataTypes.INTEGER,  
-        allowNull: false,  
-        field: 'userId',
-    },  
-    startTime: {  
-        type: DataTypes.DATE,  
-        allowNull: false,  
-    },  
-    endTime: {  
-        type: DataTypes.DATE,  
-        allowNull: false,  
-    },  
-    totalCost: {  
-        type: DataTypes.FLOAT,  
-        allowNull: false,  
-    },  
-    duration: {  // New field for duration  
-        type: DataTypes.FLOAT,  
-        allowNull: true
-    }  
-}, {  
-    sequelize,  
-    modelName: 'Booking',  
-    tableName: 'bookings', 
-    timestamps: true, // Automatically add createdAt and updatedAt fields   
-});  
+} 
 
+Booking.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    parkingLotId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    vehicleId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: 'userId',
+    },
+    startTime: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    endTime: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    totalCost: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+    },
+    duration: {
+        type: DataTypes.FLOAT,
+        allowNull: true
+    }
+}, {
+    sequelize,
+    modelName: 'Booking',
+    tableName: 'bookings',
+    timestamps: true,
+});
+ 
 module.exports = Booking;
