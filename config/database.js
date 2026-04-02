de35e1c06dc1b9587ac30aae46a7493849a9169c
makarim muhammad <jakamakarim@gmail.com>
2026-04-03 04:09:44 +0700

    feat : change db configuration to use conn string

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 const { Sequelize } = require('sequelize');  
 require('dotenv').config();  
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {  
-    host: process.env.DB_HOST,  
-    dialect: 'postgres',  
-    dialectModule: require('pg')
+const sequelize = new Sequelize(process.env.DATABASE_URL, {  
+    dialect: 'postgres',
+    dialectModule: require('pg'),
+    logging: false // Optional: suppress logs
 });  
 
 module.exports = sequelize;
