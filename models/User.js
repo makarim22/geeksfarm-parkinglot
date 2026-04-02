7e39043eff2ac2808dbc1e0419aee89deed8a8a3
makarim muhammad <jakamakarim@gmail.com>
2026-04-03 04:57:06 +0700

    feat: change default value for coins

--- models/User.js
+++ models/User.js
@@ -27,7 +27,7 @@ User.init(
     },  
     coins: {  
       type: DataTypes.FLOAT,  
-      defaultValue: 100, // Optional: set a default value  
+      defaultValue: 100000, // Optional: set a default value  
       allowNull: true, // Set this to true if you want it optional  
     },  
     roleId: {  // Foreign key to reference Role  
